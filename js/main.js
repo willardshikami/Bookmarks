171aefef191b9c69fa9a2296fed4e55da6f14e5f
willardshikami <willardshikami@gmail.com>
2017-09-26 12:31:44 +0300

    form validation

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ function saveBookmark(e) {
 	var siteName = document.getElementById('siteName').value;
 	var siteUrl = document.getElementById('siteUrl').value;
 
+if(formValidation(siteName, siteUrl)){
+	return false;
+}
+
+
 	var bookmark = {
 		name: siteName,
 		url: siteUrl
@@ -73,4 +78,21 @@ function fetchBookmarks(){
 																		'</h3>'+
 																		'</div>';
 	}
+}
+
+//form validation
+function validateForm(siteName, siteUrl){
+	if(!siteName || !siteUrl){
+		alert('Please Fill in the form');
+		return false;
+	}
+
+	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	var regex = new RegExp(expression);
+
+	if(!siteUrl.match(regex)){
+		alert('Please enter valid url');
+		return false;
+	}
+	return true;
 }
